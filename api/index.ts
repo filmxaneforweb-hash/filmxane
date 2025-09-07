d60757f97813b4bb9a6dfa4db0d0a8afbc455fb6
Serkan Ctn <serkan36900@gmail.com>
2025-09-07 15:38:32 +0300

    api/index.ts düzeltildi - async kaldırıldı, type import eklendi

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   // Basit API response
   res.status(200).json({
     message: 'Filmxane API is running!',
